a8533ac79f9117708eacbba0b0a3b3c8944c7364
FranciscoCoya <kikocoya@gmail.com>
2022-04-14 12:07:44 +0200

    Comentarios y logs en el servicio AccomodationAccServiceService.

--- api/src/main/java/com/hosting/rest/api/services/Accomodation/AccomodationAccService/AccomodationAccServiceServiceImpl.java
+++ api/src/main/java/com/hosting/rest/api/services/Accomodation/AccomodationAccService/AccomodationAccServiceServiceImpl.java
@@ -15,12 +15,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hosting.rest.api.exceptions.IllegalArguments.IllegalArgumentsCustomException;
+import com.hosting.rest.api.exceptions.NotFound.NotFoundCustomException;
 import com.hosting.rest.api.models.Accomodation.AccomodationService.AccomodationAccServiceId;
 import com.hosting.rest.api.models.Accomodation.AccomodationService.AccomodationAccServiceModel;
 import com.hosting.rest.api.models.Accomodation.AccomodationService.AccomodationServiceModel;
+import com.hosting.rest.api.repositories.Accomodation.IAccomodationRepository;
 import com.hosting.rest.api.repositories.Accomodation.AccomodationService.IAccomodationAccServiceRepo;
+import com.hosting.rest.api.repositories.Accomodation.AccomodationService.IAccomodationServiceRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author Francisco Coya · https://github.com/FranciscoCoya
+ * @version v1.0.2
+ * @description Implementa las acciones relacionadas con servicios que poseen
+ *              los alojamientos. Relación alojamiento - servicio.
+ * 
+ **/
 @Service
+@Slf4j
 public class AccomodationAccServiceServiceImpl implements IAccomodationAccServiceService {
 
 	@PersistenceContext
@@ -29,14 +43,41 @@ public class AccomodationAccServiceServiceImpl implements IAccomodationAccServic
 	@Autowired
 	private IAccomodationAccServiceRepo accomodationAccServiceRepo;
 
+	@Autowired
+	private IAccomodationRepository accomodationRepo;
+
+	@Autowired
+	private IAccomodationServiceRepository accomodationServiceRepo;
+
+	/**
+	 * Añadir el servicio <code>accomodationServiceToAdd</code> a los servicios
+	 * ofrecidos por el alojamiento con número de registro <code>regNumber</code>.
+	 * 
+	 * @param regNumber
+	 * @param accomodationServiceToAdd
+	 * 
+	 * @return
+	 */
 	@Override
 	@Transactional
 	public AccomodationAccServiceModel addNewAccomodationServiceToAccomodation(final String regNumber,
 			final AccomodationServiceModel accomodationServiceToAdd) {
 
 		if (!isNotNull(accomodationServiceToAdd)) {
+			log.error("Alguno de los valores introducidos para el servicio del alojamiento no es válido.");
 			throw new IllegalArgumentsCustomException(
 					"Alguno de los valores introducidos para el servicio del alojamiento no es válido.");
+		}
+
+		if (!isStringNotBlank(regNumber)) {
+			log.error("El número de registro de alojamiento está vacío");
+			throw new IllegalArgumentsCustomException("El número de registro de alojamiento está vacío");
+		}
+
+		// Comprobar si existe el alojamiento
+		if (!accomodationRepo.existsById(regNumber)) {
+			log.error("No existe el alojamiento con número de registro " + regNumber);
+			throw new NotFoundCustomException("No existe el alojamiento con número de registro " + regNumber);
 		}
 
 		// Clave compuesta por el número de registro del alojamiento y el id del
@@ -47,17 +88,23 @@ public class AccomodationAccServiceServiceImpl implements IAccomodationAccServic
 		return accomodationAccServiceRepo.save(new AccomodationAccServiceModel(newAccomodationServiceId));
 	}
 
+	/**
+	 * Borrado del servicio con id <code>accomodationServiceId</code> del
+	 * alojamiento con número de registro <code>accomodationRegNumber</code>.
+	 * 
+	 * @see #checkAccomodationAndServiceExists
+	 * 
+	 * @param accomodationServiceId
+	 * @param accomodationRegNumber
+	 * 
+	 * @throws NumberFormatException Si el id del servicio del alojamiento no es un
+	 *                               número.
+	 */
 	@Override
-	public void deleteServiceFromAccomodation(final Integer accomodationServiceId, final String accomodationRegNumber) {
-		if (!isStringNotBlank(accomodationRegNumber)) {
-			throw new IllegalArgumentsCustomException(
-					"El número de registro [ " + accomodationRegNumber + " ] no es válido.");
-		}
+	public void deleteServiceFromAccomodation(final Integer accomodationServiceId, final String accomodationRegNumber)
+			throws NumberFormatException {
 
-		if (!isIntegerValidAndPositive(accomodationServiceId)) {
-			throw new IllegalArgumentsCustomException(
-					"El id del servicio [ " + accomodationServiceId + " ] no es válido.");
-		}
+		checkAccomodationAndServiceExists(accomodationServiceId, accomodationRegNumber);
 
 		String deleteAccServiceByRegNumberAndAccServiceId = "DELETE FROM AccomodationAccServiceModel accsm "
 				+ " WHERE accs.accomodationAccServiceId.idAccomodation = :regNumber "
@@ -71,16 +118,27 @@ public class AccomodationAccServiceServiceImpl implements IAccomodationAccServic
 		deletedAccomodationService.executeUpdate();
 	}
 
+	/**
+	 * Obtención del sercicio con id <code>accomodationServiceId</code> del
+	 * alojamiento con número de registro <code>regNumber</code>.
+	 * 
+	 * @see #checkAccomodationAndServiceExists
+	 * 
+	 * @param accomodationServiceId
+	 * @param regNumber
+	 * 
+	 * @return
+	 * 
+	 * @throws NumberFormatException Si el id del servicio del alojamiento no es un
+	 *                               número.
+	 */
 	@Override
 	public AccomodationAccServiceModel getAccomodationAccServiceById(final Integer accomodationServiceId,
-			final String regNumber) {
+			final String regNumber) throws NumberFormatException {
 
 		AccomodationAccServiceModel accomodationServiceToReturn = null;
 
-		if (!isIntegerValidAndPositive(accomodationServiceId)) {
-			throw new IllegalArgumentsCustomException(
-					"El id del servicio del alojamiento [ " + accomodationServiceId + " ] no es válido.");
-		}
+		checkAccomodationAndServiceExists(accomodationServiceId, regNumber);
 
 		String getAccomodationAccServiceByRegNumberAndAccomodationServiceIdQuery = "SELECT accs "
 				+ "FROM AccomodationAccServiceModel accs "
@@ -100,6 +158,45 @@ public class AccomodationAccServiceServiceImpl implements IAccomodationAccServic
 		}
 
 		return accomodationServiceToReturn;
+	}
+
+	/**
+	 * Método auxiliar que comprueba que los ids de alojamiento y servicio son
+	 * válidos y existen.
+	 * 
+	 * @param accomodationServiceId
+	 * @param accomodationRegNumber
+	 * 
+	 * @throws NumberFormatException Si el id del servicio del alojamiento no es un
+	 *                               número.
+	 */
+	private void checkAccomodationAndServiceExists(final Integer accomodationServiceId,
+			final String accomodationRegNumber) throws NumberFormatException {
+
+		if (!isIntegerValidAndPositive(accomodationServiceId)) {
+			log.error("El id del servicio del alojamiento [ " + accomodationServiceId + " ] no es válido.");
+			throw new IllegalArgumentsCustomException(
+					"El id del servicio del alojamiento [ " + accomodationServiceId + " ] no es válido.");
+		}
+
+		if (!isStringNotBlank(accomodationRegNumber)) {
+			log.error("El número de registro de alojamiento está vacío");
+			throw new IllegalArgumentsCustomException("El número de registro de alojamiento está vacío");
+		}
+
+		// Comprobar si existe el servicio
+		if (!accomodationServiceRepo.existsById(accomodationServiceId)) {
+			log.error("No existe un servicio de alojamiento con id " + accomodationServiceId);
+			throw new IllegalArgumentsCustomException(
+					"No existe un servicio de alojamiento con id " + accomodationServiceId);
+		}
+
+		// Comprobar si existe el alojamiento
+		if (!accomodationRepo.existsById(accomodationRegNumber)) {
+			log.error("No existe el alojamiento con número de registro " + accomodationRegNumber);
+			throw new NotFoundCustomException(
+					"No existe el alojamiento con número de registro " + accomodationRegNumber);
+		}
 	}
 
 }
