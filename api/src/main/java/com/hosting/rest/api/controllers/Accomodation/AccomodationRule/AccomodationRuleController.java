4f160e0bb3fa47e2f43c761a06a74a01ba3d512b
FranciscoCoya <kikocoya@gmail.com>
2022-04-28 23:21:36 +0200

    CORS en controllers de booking, savedAccomodation y backup de app.log

--- api/src/main/java/com/hosting/rest/api/controllers/Accomodation/AccomodationRule/AccomodationRuleController.java
+++ api/src/main/java/com/hosting/rest/api/controllers/Accomodation/AccomodationRule/AccomodationRuleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @RestController
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/accomodations/rules")
 @Slf4j
 public class AccomodationRuleController {
